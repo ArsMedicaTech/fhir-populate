38c367105974315e92397f3f25eae4ddc3a04a72
darren277 <darren277@yahoo.com>
2025-09-27 05:00:37 -0700

    Some example encounter reasons.

---
+++ lib/data/encounter_reasons.py
@@ -0,0 +1,112 @@
+"""
+SNOMED CT Encounter Reason Codes for FHIR Appointment resources.
+Based on the HL7 FHIR ValueSet: http://hl7.org/fhir/ValueSet/encounter-reason
+"""
+
+ENCOUNTER_REASON_CODES = [
+    {
+        "code": "404684003",
+        "display": "Clinical finding (finding)",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "109006",
+        "display": "Anxiety disorder of childhood OR adolescence",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "122003",
+        "display": "Choroidal hemorrhage",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "127009",
+        "display": "Spontaneous abortion with laceration of cervix",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "129007",
+        "display": "Homoiothermia",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "134006",
+        "display": "Decreased hair growth",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "140004",
+        "display": "Chronic pharyngitis",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "144008",
+        "display": "Normal peripheral vision",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "150003",
+        "display": "Abnormal bladder continence",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "151004",
+        "display": "Gonococcal meningitis",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "162004",
+        "display": "Severe manic bipolar I disorder without psychotic features",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "165002",
+        "display": "Accident-prone",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "168000",
+        "display": "Typhlolithiasis",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "171008",
+        "display": "Injury of ascending right colon without open wound into abdominal cavity",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "172001",
+        "display": "Endometritis following molar AND/OR ectopic pregnancy",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "175004",
+        "display": "Supraorbital neuralgia",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "177007",
+        "display": "Poisoning by sawfly larvae",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "179005",
+        "display": "Apraxia of dressing",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "181007",
+        "display": "Hemorrhagic bronchopneumonia",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "183005",
+        "display": "Autoimmune pancytopenia",
+        "system": "http://snomed.info/sct"
+    },
+    {
+        "code": "185349003",
+        "display": "Encounter for check up (procedure)",
+        "system": "http://snomed.info/sct"
+    }
+]
